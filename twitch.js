4efa76f36ed922dc769ae56a348170d65e086838
Ravenbtw <ravenbtwzulul@gmail.com>
2020-12-28 19:16:47 +0000

    No longer clicks the publish button for you.

--- twitch.js
+++ twitch.js
@@ -4,11 +4,10 @@ window.onload = function() {
   var loadButton = setInterval(function() {
     if (document.querySelector('.clips-editor-slider-background.tw-mg-y-4')) {
       if (!document.querySelector('.quickChimpButton')) {
-        document.querySelector('.tw-align-items-center.tw-flex.tw-justify-content-between.tw-pd-t-1').insertAdjacentHTML('afterend','<p class="quickChimpButton" style="cursor: pointer; padding: 5px; font-size: 25px; background-color:#ff4500; text-align: center; color: white; margin-top: 10px; border-radius: 5px;">Publish and post to r/LivestreamFail</p>');
+        document.querySelector('.tw-align-items-center.tw-flex.tw-justify-content-between.tw-pd-t-1').insertAdjacentHTML('afterend','<p class="quickChimpButton" style="cursor: pointer; padding: 5px; font-size: 25px; background-color:#ff4500; text-align: center; color: white; margin-top: 10px; border-radius: 5px;">Post to r/LivestreamFail</p>');
       } else {
         clearInterval(loadButton);
         document.querySelector('.quickChimpButton').onclick = () => {
-        document.querySelector('.tw-align-items-center.tw-align-middle.tw-border-bottom-left-radius-large.tw-border-bottom-right-radius-large.tw-border-top-left-radius-large.tw-border-top-right-radius-large.tw-core-button.tw-core-button--large.tw-core-button--primary.tw-inline-flex.tw-interactive.tw-justify-content-center.tw-overflow-hidden.tw-relative').click();
           if (!document.querySelector('.tw-block.tw-border-bottom-left-radius-large.tw-border-bottom-right-radius-large.tw-border-top-left-radius-large.tw-border-top-right-radius-large.tw-font-size-5.tw-full-width.tw-input.tw-input--large.tw-pd-l-1.tw-pd-r-1.tw-pd-y-05').getAttribute('value') == '') {
             for (var i = 0; i < metaTags.length; i++) {
               if (metaTags[i].getAttribute('property') == 'og:url') {
